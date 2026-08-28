b26c9b4298bfc24f68396feb30c8ce2cc9e17703
fareezizzudinothman <fareezizzudin.othman@gmail.com>
2026-08-28 14:23:17 +0800

    terraform : add github action role and inline policy (ssm and ecr permission) (#10)

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -197,3 +197,141 @@ resource "aws_iam_instance_profile" "monitoring" {
   name = "devops-monitoring-profile"
   role = aws_iam_role.monitoring.name
 }
+
+
+# ===================================================================================
+# GitHub Actions Role
+
+# =========================================================
+# GitHub Actions OIDC Provider
+# =========================================================
+
+data "tls_certificate" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    data.tls_certificate.github_actions.certificates[0].sha1_fingerprint
+  ]
+}
+
+
+# =========================================================
+# GitHub Actions IAM Role (Create role)
+# =========================================================
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type = "Federated"
+
+      identifiers = [
+        aws_iam_openid_connect_provider.github_actions.arn
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+
+      values = [
+        "repo:fareezizzudinothman/devops-bootcamp-project:*"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "devops-github-actions-role"
+
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+
+# =========================================================
+# GitHub Actions - ECR Push Permission
+# ECR permissions
+# =========================================================
+
+resource "aws_iam_role_policy" "github_actions_ecr" {
+  name = "devops-github-actions-ecr"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "ECRAuthorization"
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Sid    = "ECRPush"
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+
+        Resource = aws_ecr_repository.app.arn
+      }
+    ]
+  })
+}
+
+# =========================================================
+# GitHub Actions - SSM Permission
+# Allows GitHub Actions to trigger deployment on Node2
+# =========================================================
+
+resource "aws_iam_role_policy" "github_actions_ssm" {
+  name = "devops-github-actions-ssm"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "SSMDeployment"
+        Effect = "Allow"
+
+        Action = [
+          "ssm:SendCommand"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
