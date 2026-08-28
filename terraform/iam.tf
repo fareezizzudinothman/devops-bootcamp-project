f95bd530a1a4b0cb39043822f6b06f81b05ba0b1
fareezizzudinothman <fareezizzudin.othman@gmail.com>
2026-08-28 14:50:52 +0800

    Terraform : change github action assume role value: need to insert repo and user ID (#12)

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -250,11 +250,11 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
 
     condition {
-      test     = "StringLike"
+      test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
-
+# kena pakai repo id dan user ID sendiri
       values = [
-        "repo:fareezizzudinothman/devops-bootcamp-project:*"
+        "repo:fareezizzudinothman@288671351/devops-bootcamp-project@1346851375:ref:refs/heads/main"
       ]
     }
   }
