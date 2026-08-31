2e11744250ef28f69fffde104f749ffd4f75262e
FareezIO <fareezizzudin.othman@gmail.com>
2026-08-31 15:53:34 +0800

    add node2 in inventory

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -5,6 +5,9 @@ resource "local_file" "ansible_inventory" {
     node1_ip = module.node1.private_ip
     node1_id = module.node1.id
 
+    node2_ip = module.node2.private_ip
+    node2_id = module.node2.id
+
     node3_ip = module.node3.private_ip
     node3_id = module.node3.id
   })
